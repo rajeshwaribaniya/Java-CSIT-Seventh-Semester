269543dbc53d5d7cdb8f71eb9344dd55ffed1dae
Rajeshwari Baniya <baniyarajeshwari@gmail.com>
2024-11-09 21:41:13 +0545

    program to demonstrate fpr-each loop

---
+++ basics/ForEachDemo.java
@@ -0,0 +1,11 @@
+public class ForEachDemo 
+{
+    public static void main(String args[])
+    {
+        int a[]= {1,2,3,4};
+        for(int i:a){
+            System.out.println(i);
+        }
+    }
+    
+}
