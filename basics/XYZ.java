e4b1d3c957932a60b711097c9b33fe04f3928f98
Rajeshwari Baniya <baniyarajeshwari@gmail.com>
2024-11-22 18:59:24 +0545

    demonstrating abstract class in Java

---
+++ basics/XYZ.java
@@ -0,0 +1,26 @@
+// Qn 3: WAP to demonstrate abstract class in Java
+
+abstract public class AbstractClassDemo 
+{
+    abstract public void m(); 
+    public void n()
+    {
+        System.out.println("Hello I am n");
+    }
+}
+
+class XYZ extends AbstractClassDemo
+{
+    public void m()
+    {
+        System.out.println("Hello I am m");
+    }
+    
+    public static void main(String args[])
+    {
+        XYZ a = new XYZ();
+        a.m();
+        a.n();
+    }
+}
+
